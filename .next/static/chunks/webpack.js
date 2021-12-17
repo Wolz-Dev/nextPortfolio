840df1ea53c06369d7c9e59e5546ebab1afc2ec1
Wolz <wolz.dev@gmail.com>
2021-12-17 16:43:42 -0300

    add second main++

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5212bd20eaa728c7"; }
+/******/ 		__webpack_require__.h = function() { return "02484764044c9a4b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
