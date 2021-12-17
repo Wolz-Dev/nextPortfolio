0e3ad75bb3bfd52751af39d4f26e7ad424f93ab4
Wolz <wolz.dev@gmail.com>
2021-12-17 19:59:43 -0300

    preparando la 4

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4ef88745dc55820a"; }
+/******/ 		__webpack_require__.h = function() { return "59d29127a59e0093"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
