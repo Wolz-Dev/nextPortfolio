a072585d499ef36866635df2dce662c81c1a62e8
Wolz <wolz.dev@gmail.com>
2021-12-17 18:55:56 -0300

    commit pre tercera parte

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "02484764044c9a4b"; }
+/******/ 		__webpack_require__.h = function() { return "9db5111473b3dde3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
