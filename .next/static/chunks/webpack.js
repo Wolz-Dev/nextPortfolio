160e112c840a1dab385258b6b810e4a50c25d03d
Wolz <wolz.dev@gmail.com>
2021-12-19 12:19:27 -0300

    tocando weass css++

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "35ad48155bb04e0e"; }
+/******/ 		__webpack_require__.h = function() { return "7bc2e43e4f95fdf6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
