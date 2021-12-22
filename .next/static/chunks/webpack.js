31ff2c6343c03aa4d20a5580d666527e6f7a4675
Wolz <wolz.dev@gmail.com>
2021-12-22 19:27:09 -0300

    preparanding css3++ daleee che3

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d2c2437bbdfa9a52"; }
+/******/ 		__webpack_require__.h = function() { return "40d7344626e9c3fe"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
