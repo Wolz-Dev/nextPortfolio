945ebba862811b65fd042472ca5fe6d8e370651f
Wolz <wolz.dev@gmail.com>
2021-12-22 14:40:45 -0300

    preparanding css2++

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c12848c63ea5c84d"; }
+/******/ 		__webpack_require__.h = function() { return "4dfbaff68921a6d5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
